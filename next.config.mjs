7dd3b0b5d9bce2b5dac10946f2b3fa1c2f658836
AShraful ISlam <ashraful.elearning.bd@gmail.com>
2026-09-01 13:04:00 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_ACTIONS === "true" || process.env.NODE_ENV === "production";
-const repoName = "nextgenifty";
-
 const nextConfig = {
-
-  trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isGithubPages ? `/${repoName}` : "",
   },
 };
 
